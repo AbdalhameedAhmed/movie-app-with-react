e1117ca34cc9d77b492dd7f14532ef127e76f04a
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-07-16 23:19:25 +0300

    feat: add tv popular section

--- src/RTK/store.js
+++ src/RTK/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import PopularSlice from "./reducers/popularMoviesSlice";
 import topratedSlice from "./reducers/topRatedMoviesSlice";
 import trendingSlice from "./reducers/trendingMoviesSlice";
+import tvPopularSlice from "./reducers/tvPopularMoviesSlice";
 
 export const store = configureStore({
   reducer: {
     popular: PopularSlice,
     toprated: topratedSlice,
     trending: trendingSlice,
+    tvPopular: tvPopularSlice,
   },
 });
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,18 +5,22 @@ import MovieSlide from "../../components/sliders/MovieSlide";
 import { fetchMovies } from "../../RTK/reducers/popularMoviesSlice";
 import { fetchTopratedMovies } from "../../RTK/reducers/topRatedMoviesSlice";
 import { fetchtrendingMovies } from "../..//RTK/reducers/trendingMoviesSlice";
+import { fetchTvPopularMovies } from "../..//RTK/reducers/tvPopularMoviesSlice";
+
 import MovieSection from "../../components/sections/MovieSection";
 
 function Home() {
   const popular = useSelector((state) => state.popular);
   const toprated = useSelector((state) => state.toprated);
   const trending = useSelector((state) => state.trending);
+  const tvPopular = useSelector((state) => state.tvPopular);
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMovies());
     dispatch(fetchTopratedMovies());
     dispatch(fetchtrendingMovies());
+    dispatch(fetchTvPopularMovies());
   }, []);
 
   return (
@@ -25,6 +29,7 @@ function Home() {
       <MovieSlide movies={popular.results} title="Popular" />
       <MovieSection movies={toprated.results} title="Top Rated" />
       <MovieSection movies={trending.results} title="Trending" />
+      <MovieSection movies={tvPopular.results} title="Tv Popular" />
     </>
   );
 }
