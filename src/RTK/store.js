b8f6537cbce034b33849bb07e9543e5964953a05
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-07-16 23:14:14 +0300

    feat: Add trending section

--- src/RTK/store.js
+++ src/RTK/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import PopularSlice from "./reducers/popularMoviesSlice";
 import topratedSlice from "./reducers/topRatedMoviesSlice";
+import trendingSlice from "./reducers/trendingMoviesSlice";
 
 export const store = configureStore({
   reducer: {
     popular: PopularSlice,
     toprated: topratedSlice,
+    trending: trendingSlice,
   },
 });
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,24 +4,27 @@ import Landing from "../../components/Home/Landing";
 import MovieSlide from "../../components/sliders/MovieSlide";
 import { fetchMovies } from "../../RTK/reducers/popularMoviesSlice";
 import { fetchTopratedMovies } from "../../RTK/reducers/topRatedMoviesSlice";
+import { fetchtrendingMovies } from "../..//RTK/reducers/trendingMoviesSlice";
 import MovieSection from "../../components/sections/MovieSection";
 
 function Home() {
   const popular = useSelector((state) => state.popular);
   const toprated = useSelector((state) => state.toprated);
+  const trending = useSelector((state) => state.trending);
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMovies());
     dispatch(fetchTopratedMovies());
+    dispatch(fetchtrendingMovies());
   }, []);
 
   return (
     <>
       <Landing />
       <MovieSlide movies={popular.results} title="Popular" />
-
       <MovieSection movies={toprated.results} title="Top Rated" />
+      <MovieSection movies={trending.results} title="Trending" />
     </>
   );
 }
